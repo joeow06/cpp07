c2f10a120098e704b575b49d907389fb83cb77a4
Ji Joe Ow <jow@u82z01s06.42kl.edu.my>
2026-05-26 14:23:45 +0800

    test: added char test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -21,6 +21,8 @@ int main(void)
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	
+	std::cout << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
@@ -29,6 +31,16 @@ int main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+	std::cout << std::endl;
+
+	char e = 'E';
+	char f = 'F';
+
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
+	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
 
 	return 0;
 }
