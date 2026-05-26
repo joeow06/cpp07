059f33f1331fd5b2c394df1eb7404c77db5ffda8
joeow06 <joeow06@gmail.com>
2026-05-27 02:25:30 +0800

    test: added char test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,8 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <exception>
 #include <iostream>
 #include "Array.hpp"
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
 
 int main()
 {
@@ -87,14 +139,14 @@ int main()
 	}
 	std::cout << "\nCopy assignment (deep copy + self-assignment)" << std::endl;
 	{
-		Array<int> a(4);	// Array a
+		Array<int> a(4);	// array a
 		for (unsigned int i = 0; i < a.size(); i++)
 			a[i] = static_cast<int>(i) + 10;
 		for (unsigned int i = 0; i < a.size(); i++)
 			std::cout << "a[" << i << "] = " << a[i] << std::endl;
 		std::cout << std::endl;
 
-		Array<int> b(1);	// Array b
+		Array<int> b(1);	// array b
 		b[0] = -1;	//modify
 		for (unsigned int i = 0; i < b.size(); i++)
 			std::cout << "b[" << i << "] = " << b[i] << std::endl;
@@ -109,6 +161,17 @@ int main()
 		std::cout << "a[1] = " << a[1] << " (should be 11)" << std::endl;
 		std::cout << "b[1] = " << b[1] << " (should be 777)" << std::endl;
 	}
-
+	std::cout << "\nArray Char Test" << std::endl;
+	{
+		Array<char> charArray(3);
+		char alphabet = 'a';
+		for (unsigned int i = 0; i < charArray.size(); i++)
+		{
+			charArray[i] = alphabet;
+			alphabet++;
+		}
+		for (unsigned int i = 0; i < charArray.size(); i++)
+			std::cout << "charArray[" << i << "] = " << charArray[i] << std::endl;
+	}
 	return 0;
 }
